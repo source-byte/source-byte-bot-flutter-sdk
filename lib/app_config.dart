e6290100aaf2aa0b141d2e20ed44f1cca949142a
msureIndosakura <msure@indosakura.com>
2025-08-18 18:39:48 +0530

    updated app config

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,4 +1,4 @@
 // Copyright (c) 2025, Indo-Sakura Software Pvt Ltd. All rights reserved.
 // Created By Suresh M, 07/08/2025
 
-const String baseUrl = String.fromEnvironment('BASE_URL');
+const String baseUrl = 'https://dev.sourcebytes.ai/api/v1';
